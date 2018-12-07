bd62d0c4177d8021005e4f348d63f8057300195d
Aylan Mello <aylanismello@gmail.com>
2018-12-07 18:40:00 +0700

    added correct plugin/setup for resizing and optimziing images from contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,19 +24,27 @@ module.exports = {
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: `gatsby-remark-images-contentful`,
             options: {
-              quality: 20,
+              quality: 10,
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 10,
+              maxWidth: 720,
+              wrapperStyle: 'height:auto;',
+              backgroundColor: 'white'
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
         ],
