a927ce0bfeb8486fd8491c45504bf89a6b21129c
Aylan Mello <aylanismello@gmail.com>
2019-02-04 19:15:30 +0800

    fixed moment date foramt bug that broke Safari date render

--- src/components/stats.js
+++ src/components/stats.js
@@ -2,15 +2,16 @@ import React from 'react';
 import moment from 'moment';
 import styles from './stats.module.css';
 
+const formattedMoment = (dateString) => moment(dateString, 'YYYY-MM-DD');
+
 // https://stackoverflow.com/questions/25150570/get-hours-difference-between-two-dates-in-moment-js
 function getNumDaysAbroad() {
-  const europe = moment.duration(moment('2018-3-18').diff(moment('2018-2-6'))).asDays();
-  const latAm = moment.duration(moment('2018-9-6').diff(moment('2018-5-12'))).asDays();
-  const asiaBeyond = moment.duration(moment(new Date()).diff(moment('2018-11-27'))).asDays();
+  const europe = moment.duration(formattedMoment('2018-3-18').diff(formattedMoment('2018-2-6'))).asDays();
+  const latAm = moment.duration(formattedMoment('2018-9-6').diff(formattedMoment('2018-5-12'))).asDays();
+  const asiaBeyond = moment.duration(formattedMoment(new Date()).diff(formattedMoment('2018-11-27'))).asDays();
 
   return Math.ceil(europe + latAm + asiaBeyond);
 }
-
 
 const Stat = ({ num, thing, emoji }) => (
   <div className={styles.statsItem}>
@@ -25,8 +26,5 @@ export default ({ numCities, numCountries }) => (
     <Stat num={numCities} thing="cities" emoji="🏙" />
     <Stat num={numCountries} thing="countries" emoji="✈️" />
     <Stat num={getNumDaysAbroad()} thing="days traveling" emoji="⏳" />
-    {/* <div className={styles.statsItem}>{numCities} cities 🏙 </div>
-    <div className={styles.statsItem}> {numCountries} countries ✈️ </div>
-    <div className={styles.statsItem}> 116 days traveling ⏳ </div> */}
   </div>
 );
